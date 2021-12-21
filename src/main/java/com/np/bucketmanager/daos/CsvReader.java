d45a1791d341a73221f30f34d816bcc2f6b3b5a7
ren <ren@nexuspad.com>
2021-12-21 15:59:52 -0500

    updated

--- src/main/java/com/np/bucketmanager/daos/CsvReader.java
+++ src/main/java/com/np/bucketmanager/daos/CsvReader.java
@@ -1,4 +1,32 @@
 package com.np.bucketmanager.daos;
 
-public class CsvReader {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class CsvReader<T> {
+    Logger logger = LoggerFactory.getLogger(CsvReader.class);
+
+    private Stream<T> dataStream;
+
+    public CsvReader(String filePath, Function<String, T> mapper) {
+        try {
+            Path path = Paths.get(filePath);
+
+            Stream<String> lines = Files.lines(path);
+            dataStream = lines.map(mapper);
+
+        } catch (InvalidPathException e) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
